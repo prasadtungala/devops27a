83df5059d08c3d9946459199403fa55768b5e564
prasaadtungala@gmail.com <prasaadtungala@gmail.com>
2025-07-27 14:56:06 +0530

    commit all proj

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b54d3615b36c979ec5711f5efd76c9e33b2eac81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f0d68b26bbdf8a0cb28cb5d6dbfc3a3622bccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
